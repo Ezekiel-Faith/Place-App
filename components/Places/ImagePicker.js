219953e8761f52400f10682da8a46686bbab403d
Ezekiel-Faith <ezekielfaith2000@gmail.com>
2026-01-26 13:20:25 +0100

    showing an image preview

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,11 +1,15 @@
-import { Alert, Button, View } from 'react-native';
+import { Alert, Button, Image, StyleSheet, Text, View } from 'react-native';
 import {
   launchCameraAsync,
   useCameraPermissions,
   PermissionStatus,
 } from 'expo-image-picker';
+import { useState } from 'react';
+import { Colors } from '../../constants/colors';
 
 export default function ImagePicker() {
+  const [pickedImage, setPickedImage] = useState();
+
   const [cameraPermissionInformation, requestPermission] =
     useCameraPermissions();
 
@@ -35,18 +39,51 @@ export default function ImagePicker() {
       return;
     }
 
+    // const image = await launchCameraAsync({
+    //   allowsEditing: true,
+    //   aspect: [16, 9],
+    //   quality: 0.5,
+    // });
+
+    // setPickedImage(image.uri);
+
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
       quality: 0.5,
     });
-    console.log(image);
+
+    if (!image.canceled) {
+      setPickedImage(image.assets[0].uri);
+    }
+  }
+
+  let imagePreview = <Text>No image taken yet.</Text>;
+
+  if (pickedImage) {
+    imagePreview = <Image style={styles.image} source={{ uri: pickedImage }} />;
   }
 
   return (
     <View>
-      <View></View>
+      <View style={styles.imagePreview}>{imagePreview}</View>
       <Button title='Take Image' onPress={takeImageHandler} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: '100%',
+    height: 200,
+    marginVertical: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
